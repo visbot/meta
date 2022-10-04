5aac4b2f75b1e5d64c1c2ef984bd9e1f49264cba
idleberg <git@idleberg.com>
2022-10-04 23:53:43 +0200

    switch to uvu

--- test/default.mjs
+++ test/default.mjs
@@ -1,10 +1,11 @@
-import test from 'ava';
+import { test } from 'uvu';
+import * as assert from 'uvu/assert';
 import { promises as fs } from 'node:fs';
 import { parse as parseYaml } from 'yaml';
 
 const dataSet = parseYaml(await fs.readFile('src/catalogue.yml', 'utf-8'));
 
-test('Unique IDs', t => {
+test('Unique IDs', () => {
     const actual = [];
     
     dataSet.forEach(item => {
@@ -13,28 +14,28 @@ test('Unique IDs', t => {
             return;
         }
 
-        t.log(`Duplicate ID found: ${item.id}`);
+        console.log(`Duplicate ID found: ${item.id}`);
     });
 
-    t.is(actual.length, dataSet.length);
+    assert.is(actual.length, dataSet.length);
 });
 
-test('Valid ID pattern', t => {
+test('Valid ID pattern', () => {
     const actual = [];
     
     dataSet.forEach(item => {
         if (!/^V(A|B|C|D|E)\d{3}(-\d)?$/.test(item.id)) {
-            t.log(`Invalid pack type found: ${item.id}`);
+            console.log(`Invalid pack type found: ${item.id}`);
             return;
         }
 
         actual.push(item);
     });
 
-    t.is(actual.length, dataSet.length);
+    assert.is(actual.length, dataSet.length);
 });
 
-test('Valid pack type', t => {
+test('Valid pack type', () => {
     const validTypes = [
         'album',
         'compilation',
@@ -45,72 +46,74 @@ test('Valid pack type', t => {
     
     dataSet.forEach(item => {
         if (!validTypes.includes(item.type)) {
-            t.log(`Invalid pack type found: ${item.id}`);
+            console.log(`Invalid pack type found: ${item.id}`);
             return;
         }
 
         actual.push(item);
     });
 
-    t.is(actual.length, dataSet.length);
+    assert.is(actual.length, dataSet.length);
 });
 
-test('Has required fields', t => {
+test('Has required fields', () => {
     const actual = [];
     
     dataSet.map((item, index) => {
         const keys = Object.keys(item);
 
         if (!keys.includes('id')) {
-            t.log(`Missing key "id" at index ${item.id ?? index}`);
+            console.log(`Missing key "id" at index ${item.id ?? index}`);
             return;
         } else if (typeof item.id !== 'string') {
-            t.log(`Invalid type for "id": ${item.id ?? index}`);
+            console.log(`Invalid type for "id": ${item.id ?? index}`);
             return;
         }
 
         if (!keys.includes('name')) {
-            t.log(`Missing key "name": ${item.id ?? index}`);
+            console.log(`Missing key "name": ${item.id ?? index}`);
             return;
         } else if (typeof item.name !== 'string') {
-            t.log(`Invalid type for "name": ${item.id ?? index}`);
+            console.log(`Invalid type for "name": ${item.id ?? index}`);
             return;
         }
 
         if (!keys.includes('type')) {
-            t.log(`Missing key "type": ${item.id ?? index}`);
+            console.log(`Missing key "type": ${item.id ?? index}`);
             return;
         } else if (typeof item.type !== 'string') {
-            t.log(`Invalid type for "type": ${item.id}`);
+            console.log(`Invalid type for "type": ${item.id}`);
             return;
         }
 
         if (!keys.includes('artists')) {
-            t.log(`Missing key "artists": ${item.id ?? index}`);
+            console.log(`Missing key "artists": ${item.id ?? index}`);
             return;
         } else if (!Array.isArray(item.artists)) {
-            t.log(`Invalid type for "type": ${item.id ?? index}`);
+            console.log(`Invalid type for "type": ${item.id ?? index}`);
             return;
         }
 
         actual.push(item);
     });
 
-    t.is(actual.length, dataSet.length);
+    assert.is(actual.length, dataSet.length);
 });
 
-test('Unique playlist IDs', t => {
+test('Unique playlist IDs', () => {
     const ids = [];
     const packWithVideo = dataSet.filter(item => item.playlist);
     
     packWithVideo.forEach(item => {
         if (ids.includes(item.id)) {
-            t.log(`Duplicate playlist ID found: ${item.id}`);
+            console.log(`Duplicate playlist ID found: ${item.id}`);
             return;
         }
 
         ids.push(item.id);
     });
 
-    t.is(ids.length, packWithVideo.length);
+    assert.is(ids.length, packWithVideo.length);
 });
+
+test.run();
